2529d8cf3ff669a82c86b368521af034f77c0345
szakul335 <szakul567@gmail.com>
2025-12-07 16:30:25 +0100

    brak konfiguratora

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,18 +8,14 @@ namespace MonitoringConfigurator.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Product> Products => Set<Product>();
-        public DbSet<Order> Orders => Set<Order>();
-        public DbSet<OrderDetail> OrderDetails => Set<OrderDetail>();
+        // USUNIÊTO: Products, Orders, OrderDetails
         public DbSet<Contact> Contacts => Set<Contact>();
         public DbSet<UserDocument> UserDocuments { get; set; } = default!;
-
-
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            builder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(18,2)");
+            // USUNIÊTO: builder.Entity<Product>().Property(p => p.Price)...
         }
     }
 }
